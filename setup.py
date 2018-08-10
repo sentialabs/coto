d05d24ee5e5e6083cbc683118703f6e52286b148
Thomas A. de Ruiter <thomas.de.ruiter@sentia.com>
2018-08-10 15:13:34 +0200

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author = "Sentia MPC B.V.",
     author_email = "info@sentia.com",
     license = "Apache",
-    version = "0.1.0",
+    version = "0.2.0",
     packages = find_packages(),
     install_requires = [
         'requests',
