9154350071db18f38341f21c49c413528d044644
Rafael Zamana <rafael.zamana.kineippe@sentia.com>
2022-03-14 15:39:13 +0100

    chore: Minor bump because of mistake

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author = "Sentia MPC B.V.",
     author_email = "info@sentia.com",
     license = "Apache",
-    version = "0.4.1",
+    version = "0.4.2",
     packages = find_packages(),
     install_requires = [
         'ansicolors==1.1.8',
